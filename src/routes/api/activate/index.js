584f7d5b83699fdb0675993b8a07d3502391f18f
ohager <oliver.hager@dextra-sw.com>
2020-11-02 11:44:58 -0300

    chore: moved activation impl to account service

--- src/routes/api/activate/index.js
+++ src/routes/api/activate/index.js
@@ -1,9 +1,9 @@
 import { serializeResponse } from '../__helpers__/serializeResponse'
-import {  } from '../../../services'
+import { accountService } from '../../../services'
 
 export async function post(req, res, next) {
     // TODO: send message
-    console.log('Sending activation message')
     res.setHeader('Content-Type', 'application/json')
     res.end(serializeResponse({ status: 'ok' }))
+    console.log('Sent activation message successfully')
 }
