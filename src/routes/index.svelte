69490d04390335167e743a2d6dd42b98dd5c5298
ohager <oliver.hager@dextra-sw.com>
2020-12-07 00:41:48 -0300

    feat: added newest burstjs version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,24 @@
     import { goto, prefetch } from '@sapper/app'
     import { RouteAccount } from '../utils/routes'
     import { account$ } from '../features/@common/accountStore'
-
     import Intro from '../features/intro/Intro.svelte'
+    import { BurstApi } from '../context'
+
+    async function test() {
+        try{
+
+        const suggestedFees = await BurstApi.network.getSuggestedFees()
+
+        const transactionId = await BurstApi.asset.cancelAskOrder({
+            order: '123',
+            senderPublicKey: 'senderPublicKey',
+            senderPrivateKey: 'senderPrivateKey',
+            feePlanck: suggestedFees.cheap,
+        })
+        }catch(e){
+            console.log(e)
+        }
+    }
 
     function handleClick() {
         goto(RouteAccount())
@@ -20,7 +36,7 @@
         <AccountFabButton on:mouseenter={prefetchRoute} on:click={handleClick} />
         <Intro />
     {:else}
-        <h1>To do Start screen for "logged" user</h1>        
+        <h1>To do Start screen for "logged" user</h1>
     {/if}
 </div>
 
